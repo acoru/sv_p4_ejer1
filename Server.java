f6d5c2115cb9eb8e58528e2c229835504b0205f3
acoru <acr00073@red.ujaen.es>
2015-10-18 19:19:49 +0200

    Added Server Class
    Initial Server Class code:
    - Just added code for the constructor in which the user must insert the
    user and the password
    - Once the user and the password has been inserted, new information can
    be added (port and domain)
    - New methods to be added later

---
+++ Server.java
@@ -0,0 +1,54 @@
+public class Server
+{
+	/*	Login class that will be use for auth in the system.
+	*	It contains multiples attributes:
+	*	user: an attribute that will contain the user name
+	*	pass: it'll contain the password of the user
+	*	domain_l: it'll will contain the domain in which the user is connecting
+	*	port: the port of the domain
+	*/
+	protected String user = "";
+	protected String pass = "";
+	protected String domain_l = "";
+	protected int port = 0;
+
+	//this is the constructor that must be called first for creating the session
+	public Server(String user, String pass)
+	{
+		this.user = user;
+		this.pass = pass;
+	}
+
+	//once the user is authenticated on the system, it can be set a domain and a port
+	public void setDomain(String domain_l)
+	{
+		this.domain_l = domain_l;
+	}
+
+	public void setPort(int port)
+	{
+		this.port = port;
+	}
+
+	//if it's necessary, user can obtain the login data.
+	public String getUser()
+	{
+		return this.user;
+	}
+
+	public String getPass()
+	{
+		return this.pass;
+	}
+
+	public int domain_l()
+	{
+		return domain_l;
+	}
+
+	//for closing the session
+	public closeSession()
+	{
+		Server.close();
+	}
+}
